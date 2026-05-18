b7df51cbf323a6fe54f76cf8944021691457ffe0
Mandeep Singh <mandeepheir11@gmail.com>
2026-05-18 13:43:55 -0400

    Added budget tracker basic setup

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
-const expenseNameInput = document.getElementById("expenseName");
-const expenseAmountInput = document.getElementById("expenseAmount");
-const addButton = document.querySelector("button");
-const expenseList = document.getElementById("expenseList");
-addButton.addEventListener("click", function() {
-    const name = expenseNameInput.value;
-    const amount = expenseAmountInput.value;
-
-    console.log(name);
-    console.log(amount);
-});
+let total = o ;
+console.log(total);
