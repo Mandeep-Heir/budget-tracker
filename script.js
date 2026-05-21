837e89ec33af819de43dd41e9377a2eac9c9c38c
Mandeep Singh <mandeepheir11@gmail.com>
2026-05-21 06:13:27 -0400

    added delete button dynamically using javascript

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let expenses = [];
+let total = 0;
 
 function AddExpense(){
     let expense = document.getElementById("expense").value;
@@ -11,16 +12,23 @@ function AddExpense(){
 // document.getElementById("output").textContent =  expense;
   //  document.getElementById("expense").value = "";  
     expenses.push(expenseObject);
+    total = total + Number(amount);
+    // document.getElementById("total").textContent = total;
     console.log(expenses);
+    document.getElementById("total").textContent = total;
   //  document.getElementById("expenseList").innerHTML =  expense;
   let list = document.getElementById("expenseList");
   list.innerHTML="";
   for(let i = 0 ; i < expenses.length; i++){
-    list.innerHTML += "<li>" 
-        + expenses[i].name 
-        + " - $" 
-        + expenses[i].amount 
-        + "</li>";
+   list.innerHTML += "<li>" 
+    + expenses[i].name 
+    + " - $" 
+    + expenses[i].amount
+    + " <button>Delete</button>"
+    + "</li>";
   }
 
+  document.getElementById("expense").value = "";
+  document.getElementById("amount").value = "";
+  //document.getElementById("total").textContent = total;
 }
