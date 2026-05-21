f815c3a5acde0960535fc2da82ce2bdcb6a84f1f
Mandeep Singh <mandeepheir11@gmail.com>
2026-05-21 07:50:32 -0400

    updated script

--- script.js
+++ script.js
@@ -4,6 +4,14 @@ let total = 0;
 function AddExpense(){
     let expense = document.getElementById("expense").value;
     let amount = document.getElementById("amount").value;
+    if(expense === "" || amount === ""){
+        alert("Please enter both expense and amount.");
+        return;
+    }
+    if (isNaN(amount) || Number(amount) <= 0){
+        alert("Please enter a valid amount.");
+        return;
+    }
     let expenseObject={
       name: expense,
       amount: amount
@@ -16,6 +24,7 @@ function AddExpense(){
     // document.getElementById("total").textContent = total;
     console.log(expenses);
     document.getElementById("total").textContent = total;
+    document.getElementById("count").textContent = expenses.length;
   //  document.getElementById("expenseList").innerHTML =  expense;
   let list = document.getElementById("expenseList");
   list.innerHTML="";
@@ -24,11 +33,32 @@ function AddExpense(){
     + expenses[i].name 
     + " - $" 
     + expenses[i].amount
-    + " <button>Delete</button>"
+    + " <button onclick='DeleteExpense(" + i + ")'>Delete</button>"
     + "</li>";
   }
 
   document.getElementById("expense").value = "";
   document.getElementById("amount").value = "";
   //document.getElementById("total").textContent = total;
+//   function DeleteExpense(index){
+//     alert(index);
+// }
+
+}
+  function DeleteExpense(index){
+  total = total - Number(expenses[index].amount);
+  document.getElementById("total").textContent = total;
+   expenses.splice(index,1);
+   let list = document.getElementById("expenseList");
+list.innerHTML = "";
+
+for (let i = 0; i < expenses.length; i++) {
+    list.innerHTML += "<li>"
+    + expenses[i].name
+    + " - $"
+    + expenses[i].amount
+    + " <button onclick='DeleteExpense(" + i + ")'>Delete</button>"
+    + "</li>";
+}
+  
 }
