894d4ca51b5019948d1109ea76cc540715ecc82f
Mandeep Singh <mandeepheir11@gmail.com>
2026-05-21 00:01:46 -0400

    Added expense list loop and object learning

--- script.js
+++ script.js
@@ -2,16 +2,25 @@ let expenses = [];
 
 function AddExpense(){
     let expense = document.getElementById("expense").value;
+    let amount = document.getElementById("amount").value;
+    let expenseObject={
+      name: expense,
+      amount: amount
+    };
    // alert(expense);
 // document.getElementById("output").textContent =  expense;
   //  document.getElementById("expense").value = "";  
-    expenses.push(expense);
+    expenses.push(expenseObject);
     console.log(expenses);
   //  document.getElementById("expenseList").innerHTML =  expense;
   let list = document.getElementById("expenseList");
   list.innerHTML="";
   for(let i = 0 ; i < expenses.length; i++){
-    list.innerHTML+="<li>"  + expenses[i] +"</li>";
+    list.innerHTML += "<li>" 
+        + expenses[i].name 
+        + " - $" 
+        + expenses[i].amount 
+        + "</li>";
   }
 
 }
